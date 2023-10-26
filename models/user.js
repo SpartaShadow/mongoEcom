fc5b8d0cdead76bf69ed1db3e1543cf4247bd29c
SpartaShadow <114387187+SpartaShadow@users.noreply.github.com>
2023-10-27 04:41:56 +0530

    added cart with product in user

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(name, email) {
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
+    this.cart = cart; // {items : []}
+    this._id = id;
   }
 
   save() {
@@ -14,6 +16,22 @@ class User {
       .insertOne(this)
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
+  }
+
+  addToCart(product) {
+    // const cartProduct=this.cart.items.findIndex(ele=>
+    //   {
+    //     return ele._id == product._id;
+    //   });
+    const updatedCart = {
+      items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }],
+    };
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(id) {
