76cbc98282f5048d08d9b9a70a6c108ee7f85725
SpartaShadow <114387187+SpartaShadow@users.noreply.github.com>
2023-10-27 04:48:03 +0530

    added order functioning

--- controllers/shop.js
+++ controllers/shop.js
@@ -116,10 +116,21 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+    .addOrder()
+    .then((result) => res.redirect("/orders"))
+    .catch((err) => console.log(err));
+};
+
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
+  req.user.getOrders().then((orders) => {
+    res.render("shop/orders", {
+      path: "/orders",
+      pageTitle: "Your Orders",
+      orders: orders,
+    });
   });
 };
 
